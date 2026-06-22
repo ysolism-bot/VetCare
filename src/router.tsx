898b065fa5687a8df85e01266d1b4cbed8a11568
Yuriana Solis <ysolism@undac.edu.pe>
2026-06-22 12:48:08 -0500

    fix getRouter export

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,10 @@
 import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-export const router = createRouter({
-  routeTree,
-  scrollRestoration: true,
-  defaultPreloadStaleTime: 0,
-})
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+export function getRouter() {
+  return createRouter({
+    routeTree,
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  })
 }
