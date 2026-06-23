3b98ef2bfca580e1b2079fa47346f581841da676
Yuriana Solis <ysolism@undac.edu.pe>
2026-06-22 19:53:59 -0500

    fix package.json y conexion BD

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,6 @@
-import { drizzle } from 'drizzle-orm/netlify-db'
+import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema.js'
 
-export const db = drizzle({ schema })
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle(sql, { schema })
